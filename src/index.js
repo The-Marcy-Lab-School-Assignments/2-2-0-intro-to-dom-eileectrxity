887c2de5be0f5c25f3833d4c3951788e92915df0
electrxety <er191712@gmail.com>
2024-06-20 19:28:46 -0400

    solved q9 p2: add p

--- src/index.js
+++ src/index.js
@@ -1,4 +1,4 @@
-/* in creating the index.html file, index.js file must -->
+/* QUESTION 9: fromScratch- in creating the index.html file, index.js file must -->
 1) add an H1 to the body with an id of main-heading and text of "Hello World!"
 2) add a p tag with an id of main-text, a class of boring-text, and text that reads "Look, I'm some text!" */
 
@@ -11,9 +11,29 @@ const addH1 = () => {
   document.body.appendChild(h1); //appending new el to the document body
 };
 
+const addP = () => {
+  const p = document.createElement('p'); //creating a p tag node el
+  Object.assign(p, {
+    id: "main-text", //setting the id attribute
+    className: "boring-text", //setting the class attribute by the className property
+    textContent: "Look, I'm some text!"
+  })
+  document.body.appendChild(p);
+};
+
+// //alt method
+// const addP = () => {
+//   const p = document.createElement("p");
+//   p.id = "main-text";
+//   p.classList.add("boring-text");
+//   p.textContent = "Look, I'm some text!";
+//   document.body.appendChild(p);
+// };
+
 //runner function
 const main = () => {
   addH1(); //create
+  addP(); //create
 };
 
 main(); //invoking runner function so that it renders in the browser when the dom is fully parsed + loaded from source file index.html that includes this index.js file as a script
